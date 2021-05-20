f22d3ff835cbc2bfd9a968ac6f3416dd44249e71
Sarthak Jain <54719418+XSarthakJain@users.noreply.github.com>
2021-05-20 23:44:55 +0530

    Header.jsx change

--- skatch/src/Header.jsx
+++ skatch/src/Header.jsx
@@ -16,22 +16,26 @@ function Header(){
                 <label id="LeftHeaderMenuicon" onClick={sideMenuAppear}>
                     <i className="material-icons">menu</i>
                 </label>
+
+                <NavLink exact to="/skatch"><label className="Headerlogo" skatch-tooltip="Skatch"><a href="javascript:void(0)">Skatch</a></label></NavLink>
+
                 <NavLink exact to="/"><label className="Headerlogo" skatch-tooltip="Skatch"><a href="javascript:void(0)">Skatch</a></label></NavLink>
+
             </header>
                 <div className="leftHeaderMenuOptions">
                     <ul>
                         <li className="SkatchGithubRepo"><a href="https://github.com/XSarthakJain/skatch.github.io" ><i className="fa fa-github"></i></a></li>
-                        <NavLink exact to="gettingstarted"><li><a >Getting Started</a></li></NavLink>
-                        <NavLink exact to="alert"><li><a >Alert</a></li></NavLink>
-                        <NavLink exact to="badge"><li><a >Badge</a></li></NavLink>
-                        <NavLink exact to="button"><li><a >Button</a></li></NavLink>
-                        <NavLink exact to="card"><li><a>Card</a></li></NavLink>
-                        <NavLink exact to="footer"><li><a>Footer</a></li></NavLink>
-                        <NavLink exact to="navbar"><li><a>Header</a></li></NavLink>
-                        <NavLink exact to="popup"><li><a>PopUp</a></li></NavLink>
-                        <NavLink exact to="toast"><li><a>Toast</a></li></NavLink>
-                        <NavLink exact to="tooltip"><li><a>Tooltips</a></li></NavLink>
-                        <NavLink exact to="form"><li><a>Form</a></li></NavLink>
+                        <NavLink exact to="/skatch/gettingstarted"><li><a >Getting Started</a></li></NavLink>
+                        <NavLink exact to="/skatch/alert"><li><a >Alert</a></li></NavLink>
+                        <NavLink exact to="/skatch/badge"><li><a >Badge</a></li></NavLink>
+                        <NavLink exact to="/skatch/button"><li><a >Button</a></li></NavLink>
+                        <NavLink exact to="/skatch/card"><li><a>Card</a></li></NavLink>
+                        <NavLink exact to="/skatch/footer"><li><a>Footer</a></li></NavLink>
+                        <NavLink exact to="/skatch/navbar"><li><a>Header</a></li></NavLink>
+                        <NavLink exact to="/skatch/popup"><li><a>PopUp</a></li></NavLink>
+                        <NavLink exact to="/skatch/toast"><li><a>Toast</a></li></NavLink>
+                        <NavLink exact to="/skatch/tooltip"><li><a>Tooltips</a></li></NavLink>
+                        <NavLink exact to="/skatch/form"><li><a>Form</a></li></NavLink>
                     </ul>
                 </div>
             {/* <!-- End Header --> */}
